0005ebecf8d9de7fa129d43b50e8d87d7533d456
kms20a <kms21@naver.com>
2021-01-01 21:25:13 +0900

    02. 네비게이션 메뉴(타임리프 레이아웃)

---
+++ src/main/java/com/mskim/demo/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.mskim.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+    // /member
+    @GetMapping
+    public String index(){
+        return "member";
+    }
+}
