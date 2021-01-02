817380102c106b5fd6cbfacf3fa54aefd7fc4f9c
kms20a <kms21@naver.com>
2021-01-02 13:27:01 +0900

    05. Member - Paging 처리

--- src/main/java/com/mskim/demo/service/MemberService.java
+++ src/main/java/com/mskim/demo/service/MemberService.java
@@ -1,8 +1,11 @@
 package com.mskim.demo.service;
 
 import com.mskim.demo.domain.Member;
+import com.mskim.demo.helper.CommonPageInfo;
 import com.mskim.demo.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -20,5 +23,19 @@ public class MemberService {
 
     public Member save(Member member) {
         return repository.save(member);
+    }
+
+    public void createDummy() {
+        for(int i = 0; i < 200; i++){
+            Member member = new Member();
+            member.setUsername("user" + i + "@naver.com");
+            member.setPassword("1234");
+            repository.save(member);
+        }
+    }
+
+    public CommonPageInfo<Member> findAll(Pageable pageable) {
+        Page<Member> all = repository.findAll(pageable);
+        return new CommonPageInfo<Member>(all);
     }
 }
